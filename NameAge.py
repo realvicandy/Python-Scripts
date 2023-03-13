5659f853848d7237e9b374075738489c36a75e86
Victor <victor@Victors-MacBook-Pro.local>
2023-03-13 14:30:59 -0700

    Adding description to comment

--- NameAge.py
+++ NameAge.py
@@ -1,5 +1,13 @@
 import datetime
 
+# This is a simple Python script that takes your name and age and outputs your birth year.
+#
+# I completed it an assignment for IT140 Intro to Scripting at Southern New Hampshire University
+#
+# Note: if your birthday for the current year has not yet occured then the output year will be one year greater
+#       than it should be. This is how the script had to be written to correctly answer what the prompt was requesting.
+
+
 # Getting current year
 current_year = datetime.date.today().year
 
